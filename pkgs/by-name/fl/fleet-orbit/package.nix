2c4c7c7516b2034b68449997a55eb0de88666a22
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-07-23 00:37:11 -0400

    fleet-orbit: integrate Nix-managed runtime paths

--- pkgs/by-name/fl/fleet-orbit/package.nix
+++ pkgs/by-name/fl/fleet-orbit/package.nix
@@ -1,10 +1,13 @@
 {
+  bash,
   lib,
   buildGoModule,
   fetchFromGitHub,
   nix-update-script,
   nixosTests,
+  python3,
   versionCheckHook,
+  zsh,
 }:
 
 buildGoModule (finalAttrs: {
@@ -33,14 +36,23 @@ buildGoModule (finalAttrs: {
     "-X=github.com/fleetdm/fleet/v4/orbit/pkg/build.Version=${finalAttrs.version}"
     "-X=github.com/fleetdm/fleet/v4/orbit/pkg/build.Commit=0000000000000000000000000000000000000000"
     "-X=github.com/fleetdm/fleet/v4/orbit/pkg/build.Date=1970-01-01T00:00:00Z"
+    "-X=github.com/fleetdm/fleet/v4/orbit/pkg/scripts.scriptShPath=${lib.getExe' bash "sh"}"
+    "-X=github.com/fleetdm/fleet/v4/orbit/pkg/scripts.scriptBashPath=${lib.getExe bash}"
+    "-X=github.com/fleetdm/fleet/v4/orbit/pkg/scripts.scriptZshPath=${lib.getExe zsh}"
+    "-X=github.com/fleetdm/fleet/v4/orbit/pkg/scripts.scriptPythonPath=${lib.getExe python3}"
+    "-X=github.com/fleetdm/fleet/v4/orbit/pkg/scripts.scriptPython3Path=${lib.getExe python3}"
   ];
 
   patches = [
-    ./0001-orbit-nixos.patch
-    ./0002-osqueryd-path-override.patch
-    ./0003-osquery-log-path.patch
-    ./0004-scripts-nixos.patch
+    ./0001-runtime-path-overrides.patch
+    ./0002-script-interpreter-paths.patch
   ];
+
+  preCheck = ''
+    go test ./orbit/cmd/orbit \
+      -run '^(TestExternalComponentPaths|TestConfiguredPath|TestLinuxBrowserPath)$'
+    go test ./orbit/pkg/scripts -run '^TestRewriteShebang$'
+  '';
 
   doInstallCheck = true;
   versionCheckProgramArg = "version";
